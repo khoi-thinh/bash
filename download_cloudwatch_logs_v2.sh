1b78c3717222849e97a5e80143d6c53473fe2450
Khoi Thinh <thinhduckhoi@gmail.com>
2024-06-17 10:14:06 +0900

    Create download_cloudwatch_logs_v2.sh

---
+++ download_cloudwatch_logs_v2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get event logs based on a specific range date
+
+# define output 
+outfile="C:\Users\khoi.thinh\bash_scripts\cloudwatch_$(date +%F).log"
+
+# define log group name e.g. "/aws/lambda/condeco-employee-data-file-transfer-lambda-prod-s3hook"
+echo "Enter a log group name"
+read log_group_name
+
+# Define start and end time range
+echo "Enter the start time in yyyy-mm-dd HH:MM:SS"
+read start_time
+
+echo "Enter end time in yyyy-mm-dd HH:MM:SS format" 
+read end_time
+
+# convert to epoch time in milliseconds
+start_epoch_time=$(date -d "${start_time}" +%s%3N)
+end_epoch_time=$(date -d "${end_time}" +%s%3N)
+
+# Get log events
+MSYS_NO_PATHCONV=1 aws logs filter-log-events --log-group-name "$log_group_name" --start-time $start_epoch_time --end-time $end_epoch_time --output text > $outfile
